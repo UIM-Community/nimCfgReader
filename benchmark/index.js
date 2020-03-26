acbbb1cf203d2fb9c0396e19fef691ef07b2b73d
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2020-03-27 00:00:53 +0100

    fix: benchmark

--- benchmark/index.js
+++ benchmark/index.js
@@ -1,9 +1,10 @@
 const Benchmark = require('benchmark');
 const nimCfgReader = require('../index');
 const { readFileSync } = require('fs');
+const { join } = require("path");
 
 const suite = new Benchmark.Suite;
-const sample = readFileSync('./sample.cfg').toString();
+const sample = readFileSync(join(__dirname, "sample.cfg"), "utf-8");
 
 // add tests 
 suite.add('PDS (File Buffer) Parsing', () => {
